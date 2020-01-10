ca1276f092d6bfdc128fe58d0a5792c760646a80
Sebastian Perez <sperezm97@gmail.com>
2020-01-10 11:01:15 -0400

    refactor async methods to sync

--- android/src/main/java/com/reactlibrary/Api.java
+++ android/src/main/java/com/reactlibrary/Api.java
@@ -1,13 +1,10 @@
-package com.reactlibrary;
+package com.example.prueba;
 
 import android.os.StrictMode;
 
-import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -25,6 +22,8 @@ public class Api  {
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     private OkHttpClient client = new OkHttpClient();
     private String url = "https://jsonplaceholder.typicode.com/posts";
+
+
     private String value;
     private String baseUrl = "https://api.worldmobileconnection.com/v2";
     private String gw;
@@ -38,25 +37,69 @@ public class Api  {
 
     public Api(String key) {
         apiKey = key;
-        getToken();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
-
 
     public String getValue() {
         return value;
     }
 
-    private void getValueFromRequest(String response) {
-        value = response;
+    public void setValue(String value) {
+        this.value = value;
     }
 
+    private JSONObject createBody() {
+        JSONObject body = new JSONObject();
+        try {
+            body.put("token", apiToken)
+                    .put("mcc", "370")
+                    .put("mnc", "02")
+                    .put("uniqid", "123456")
+                    .put("transport", "mobile");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return body;
+    }
+
+    private void fetchUser() {
+        String endpoint = "/getuser";
+
+        Request request = new Request.Builder()
+                .url(baseUrl + endpoint)
+                .post(RequestBody.create(JSON, createBody().toString() ))
+                .build();
+
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()){
+                setUser(response.body().string());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setUser(String value) {
+        try {
+            JSONObject jsonObject = new JSONObject(value);
+            gw = jsonObject.getString("gw").substring(0,39);
+            port = Integer.parseInt(jsonObject.getString("gw").substring(40));
+            username = jsonObject.getString("username");
+            password = jsonObject.getString("password");
+            sessionid = jsonObject.getString("sessionid");
+            expire_seconds = jsonObject.getString("expire_seconds");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public void run(){
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
-        StrictMode.setThreadPolicy(policy);
-
 
         Request request = new Request.Builder()
                 .url(url)
@@ -81,52 +124,44 @@ public class Api  {
 
         client.newBuilder().proxy(proxy).build();
 
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                e.printStackTrace();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful() ) {
+                setValue(response.body().string());
 
             }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if (response.isSuccessful() ) {
-                    getValueFromRequest(response.body().string());
-
-                }
-            }
-        });
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 
     public void getToken() {
         String endpoint = "/gettoken";
-        String body = "{\"apiKey\":\""+ apiKey +"\"}";
+        JSONObject body = new JSONObject();
+
+        try {
+            body.put("apikey", apiKey);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
         Request request = new Request.Builder()
                 .url(baseUrl + endpoint)
-                .post(RequestBody.create(JSON, body))
+                .post(RequestBody.create(JSON, body.toString()))
                 .build();
 
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                e.printStackTrace();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                saveToken(response.body().string());
+                fetchUser();
             }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    saveToken(response.body().string());
-                    getUser();
-
-                }
-            }
-        });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
-
 
     private void saveToken(String value) {
         try {
@@ -136,61 +171,5 @@ public class Api  {
             e.printStackTrace();
         }
     }
-
-    private JSONObject createBody() {
-        JSONObject body = new JSONObject();
-        try {
-            body.put("token", apiToken)
-                    .put("mcc", "370")
-                    .put("mnc", "02")
-                    .put("uniqid", "123456")
-                    .put("transport", "mobile");
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return body;
-    }
-
-    private void getUser() {
-        String endpoint = "/getuser";
-
-
-        Request request = new Request.Builder()
-                .url(baseUrl + endpoint)
-                .post(RequestBody.create(JSON, ""+ createBody() +"" ))
-                .build();
-
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if (response.isSuccessful()){
-                    saveUser(response.body().string());
-
-                }
-            }
-        });
-
-    }
-
-    private void saveUser(String value) {
-        try {
-            JSONObject jsonObject = new JSONObject(value);
-            gw = jsonObject.getString("gw").substring(0,39);
-            port = Integer.parseInt(jsonObject.getString("gw").substring(40));
-            username = jsonObject.getString("username");
-            password = jsonObject.getString("password");
-            sessionid = jsonObject.getString("sessionid");
-            expire_seconds = jsonObject.getString("expire_seconds");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
+
