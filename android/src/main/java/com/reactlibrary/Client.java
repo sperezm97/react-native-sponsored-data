f19b45f6155191be2c89aa459158dcdae574daa7
Sebastian Perez <sperezm97@gmail.com>
2020-01-15 14:49:38 -0400

    Add Client Api to handle call apis

---
+++ android/src/main/java/com/reactlibrary/Client.java
@@ -0,0 +1,236 @@
+package com.example.prueba.Refactor;
+
+import android.os.StrictMode;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.PasswordAuthentication;
+import java.net.Proxy;
+
+interface IClient {
+    String get(String url);
+    String put(String url, String id, JSONObject body);
+    void delete(String url, String id);
+    String post(String url, JSONObject body);
+
+}
+
+public class Client implements IClient{
+
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    private OkHttpClient client = new OkHttpClient();
+    final SponsoredData sponsored = SponsoredData.getInstance();
+    InetSocketAddress proxyAddr;
+    Proxy proxy;
+    private static Client single_instance = null;
+
+    public Client() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public static Client getInstance()
+    {
+        if (single_instance == null) {
+            single_instance = new Client();
+        }
+
+        return single_instance;
+    }
+
+    @Override
+    public String get(String url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                if (getRequestingHost().equalsIgnoreCase(sponsored.getProxyBaseURL())) {
+                    if (sponsored.getProxyPort() == getRequestingPort()) {
+                        return new PasswordAuthentication(sponsored.getProxyUsername(), sponsored.getProxyPassword().toCharArray());
+                    }
+                }
+                return null;
+            }
+        });
+        client.newBuilder().proxy(proxy).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public String put(String url, @NotNull JSONObject body) {
+        Request request = new Request.Builder()
+                .url(url)
+                .put(RequestBody.create(JSON, body.toString()))
+                .build();
+
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                if (getRequestingHost().equalsIgnoreCase(sponsored.getProxyBaseURL())) {
+                    if (sponsored.getProxyPort() == getRequestingPort()) {
+                        return new PasswordAuthentication(sponsored.getProxyUsername(), sponsored.getProxyPassword().toCharArray());
+                    }
+                }
+                return null;
+            }
+        });
+        client.newBuilder().proxy(proxy).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public String post(String url, @NotNull JSONObject body) {
+        Request request = new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(JSON, body.toString()))
+                .build();
+
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                if (getRequestingHost().equalsIgnoreCase(sponsored.getProxyBaseURL())) {
+                    if (sponsored.getProxyPort() == getRequestingPort()) {
+                        return new PasswordAuthentication(sponsored.getProxyUsername(), sponsored.getProxyPassword().toCharArray());
+                    }
+                }
+                return null;
+            }
+        });
+        client.newBuilder().proxy(proxy).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void delete(String url, String id) {
+        Request request = new Request.Builder()
+                .url(url + id)
+                .delete()
+                .build();
+
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                if (getRequestingHost().equalsIgnoreCase(sponsored.getProxyBaseURL())) {
+                    if (sponsored.getProxyPort() == getRequestingPort()) {
+                        return new PasswordAuthentication(sponsored.getProxyUsername(), sponsored.getProxyPassword().toCharArray());
+                    }
+                }
+                return null;
+            }
+        });
+        client.newBuilder().proxy(proxy).build();
+
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getTokenWMC() {
+        JSONObject body = null;
+        try {
+            body = new JSONObject()
+                    .put("apiKey", sponsored.getApiKey());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        Request request = new Request.Builder()
+                .url("https://api.worldmobileconnection.com/v2/gettoken")
+                .post(RequestBody.create(JSON, body.toString()))
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                String result = response.body().string();
+                sponsored.setApiToken(new JSONObject(result).getString("Token"));
+                getUserInfoWMC();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getUserInfoWMC() {
+        JSONObject body = null;
+        try {
+            body = sponsored.createUserBody();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Request request = new Request.Builder()
+                .url("https://api.worldmobileconnection.com/v2/getuser")
+                .post(RequestBody.create(JSON, body.toString()))
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                String result = response.body().string();
+                sponsored.setUser(result);
+                setProxyWMC();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setProxyWMC() {
+        proxyAddr = new InetSocketAddress(sponsored.getProxyBaseURL(), sponsored.getProxyPort());
+        proxy = new Proxy(Proxy.Type.HTTP, proxyAddr.createUnresolved(sponsored.getProxyBaseURL(), sponsored.getProxyPort()));
+    }
+}
