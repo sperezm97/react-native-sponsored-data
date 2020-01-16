386241d79f86a63f1bb9c406e39503549131d4b4
Sebastian Perez <sperezm97@gmail.com>
2020-01-16 19:35:17 -0400

    Fix missing methods

--- android/src/main/java/com/reactlibrary/MapUtil.java
+++ android/src/main/java/com/reactlibrary/MapUtil.java
@@ -14,8 +14,6 @@ import com.facebook.react.bridge.WritableMap;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.lang.Object;
-
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -56,6 +54,61 @@ public class MapUtil {
 
     return jsonObject;
   }
+
+  public static Map<String, Object> toMap(JSONObject jsonObject) throws JSONException {
+    Map<String, Object> map = new HashMap<>();
+    Iterator<String> iterator = jsonObject.keys();
+
+    while (iterator.hasNext()) {
+      String key = iterator.next();
+      Object value = jsonObject.get(key);
+
+      if (value instanceof JSONObject) {
+        value = MapUtil.toMap((JSONObject) value);
+      }
+      if (value instanceof JSONArray) {
+        value = ArrayUtil.toArray((JSONArray) value);
+      }
+
+      map.put(key, value);
+    }
+
+    return map;
+  }
+
+  public static Map<String, Object> toMap(ReadableMap readableMap) {
+    Map<String, Object> map = new HashMap<>();
+    ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+
+    while (iterator.hasNextKey()) {
+      String key = iterator.nextKey();
+      ReadableType type = readableMap.getType(key);
+
+      switch (type) {
+        case Null:
+          map.put(key, null);
+          break;
+        case Boolean:
+          map.put(key, readableMap.getBoolean(key));
+          break;
+        case Number:
+          map.put(key, readableMap.getDouble(key));
+          break;
+        case String:
+          map.put(key, readableMap.getString(key));
+          break;
+        case Map:
+          map.put(key, MapUtil.toMap(readableMap.getMap(key)));
+          break;
+        case Array:
+          map.put(key, ArrayUtil.toArray(readableMap.getArray(key)));
+          break;
+      }
+    }
+
+    return map;
+  }
+
   public static WritableMap toWritableMap(Map<String, Object> map) {
     WritableMap writableMap = Arguments.createMap();
     Iterator iterator = map.entrySet().iterator();
